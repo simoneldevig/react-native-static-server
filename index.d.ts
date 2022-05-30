1771a5173ea51b63058b8f2047451d6fe28eb00a
William Snowdon <william.snowdon@macbook.local>
2022-05-30 14:12:41 +0100

    fix TS module declaration for fork

--- index.d.ts
+++ index.d.ts
@@ -1,22 +1,22 @@
-declare module 'react-native-static-server' {
+declare module '@dr.pogodin/react-native-static-server' {
   type Options = {
-    localOnly?: boolean
-    keepAlive?: boolean
-  }
+    localOnly?: boolean;
+    keepAlive?: boolean;
+  };
 
   export default class StaticServer {
-    constructor(port: number, root?: string, opts?: Options)
+    constructor(port: number, root?: string, opts?: Options);
 
-    port: number
-    root: string
-    localOnly: boolean
-    keepAlive: boolean
-    started: boolean
-    _origin?: string
+    port: number;
+    root: string;
+    localOnly: boolean;
+    keepAlive: boolean;
+    started: boolean;
+    _origin?: string;
 
-    start: () => Promise<string>
-    stop: () => Promise<any>
-    isRunning: () => Promise<boolean>
-    kill: () => void
+    start: () => Promise<string>;
+    stop: () => Promise<any>;
+    isRunning: () => Promise<boolean>;
+    kill: () => void;
   }
 }
