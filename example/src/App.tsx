240c22051bb29444d9f62b72224f8078b2366a74
Dr. Sergey Pogodin <dr.pogodin@gmail.com>
2023-02-07 11:29:33 +0100

    Adds Windows template into example app

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -15,7 +15,14 @@ import {
 
 import { Colors } from 'react-native/Libraries/NewAppScreen';
 
+// TODO: rn-fs does not work on Windows out of the box,
+// which is a know issue with a known workaround:
+// https://github.com/microsoft/react-native-windows/issues/2829
+// The workaround has been applied to this example app for now,
+// but it would be nice to figure out why it has not been fixed
+// in rn-fs itself?
 import RNFS from 'react-native-fs';
+
 import { WebView } from 'react-native-webview';
 
 import Server, {
